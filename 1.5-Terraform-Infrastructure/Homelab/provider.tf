30af6e98d26836ed2282663655f0859df884a8d6
initcyber <justin@initcyber.com>
2024-07-04 08:29:14 -0400

    Updating for Terraform

---
+++ 1.5-Terraform-Infrastructure/Homelab/provider.tf
@@ -0,0 +1,43 @@
+# Proxmox Provider
+# ---
+# Initial Provider Configuration for Proxmox
+
+terraform {
+
+    required_version = ">= 0.13.0"
+
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "3.0.1-rc3" # Latest as of 7/4/2024
+        }
+    }
+}
+
+# Variables below defined in credential.auto.tfvars
+variable "proxmox_api_url" {
+    type = string
+}
+
+variable "proxmox_api_token_id" {
+    type = string
+}
+
+variable "proxmox_api_token_secret" {
+    type = string
+}
+
+variable "ubuntu_24_template" {
+    type = string
+}
+
+provider "proxmox" {
+
+    pm_api_url = var.proxmox_api_url
+    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_token_secret = var.proxmox_api_token_secret
+
+    # (Optional) Skip TLS Verification - uncomment for self signed certs
+    # pm_tls_insecure = true
+
+}
---
+++ 1.5-Terraform-Infrastructure/Homelab/ubuntu-clone.tf
@@ -0,0 +1,54 @@
+# Proxmox Full-Clone for Ubuntu VM's (Ubuntu 22.04 cloud-init)
+# ---
+# This will create a new Virtual Machine from a cloud-init file
+
+resource "proxmox_vm_qemu" "ubuntu-vm" {
+    
+    #Set this number to how many VM's you need to deploy, comment out if you don't need to deploy more than 1 (adjust "vmid" and "name" as needed)
+    count = 1
+    # List our target node (this is the node ID of our "cluster")
+    # vmid is the virtual machine ID in Proxmox, default starts at 100 and counts up
+    # name is the name we will identify our virtual machine as
+    # desc is a descriptive name for our virtual machine
+    target_node = "discovery"
+    vmid = "20${count.index + 1}"
+    name = "ubuntu-vm-0${count.index + 1}"
+    desc = "Testing out virtual machines"
+
+    # Set VM to start on boot (true/false)
+    onboot = true 
+
+    # We are cloning this template identified here - This is a variable identified in credentials.auto.tfvars
+    clone = var.ubuntu_24_template
+
+    # Set to 1 to enable the QEMU Guest Agent.
+    agent = 1
+    
+    # VM CPU settings - self explanatory
+    cores = 2
+    sockets = 1
+    cpu = "host"    
+    
+    # VM Memory Settings - Again, self explantory
+    memory = 2048
+
+    # VM Network Settings - Same
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    # Default to cloud-init
+    os_type = "cloud-init"
+
+    # IP Address and Gateway - Again, we are using the count.index variable here, assuming we are NOT going above 10 virtual machines this should be OK.
+    ipconfig0 = "ip=172.16.10.6${count.index + 1}/24,gw=172.16.10.1"
+    
+    # Set user name here
+    # ciuser = "your-username"
+    # ---
+    # Set SSH keys here
+    # sshkeys = <<EOF
+    # #YOUR-PUBLIC-SSH-KEY
+    # EOF
+}
